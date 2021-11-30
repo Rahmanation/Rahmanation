5de9d1042f561f0b9ea20f4b3e7e1ff7ecd0cc8c
Rahmanation <94148891+Rahmanation@users.noreply.github.com>
2021-11-30 11:46:58 +0000

    Week 1: basic Java Script Problem solving

---
+++ elseif.js
@@ -0,0 +1,9 @@
+let weather = "sunny";
+if (weather == "sunny"){
+  console.log ("Well I better wear some Sunscream!");
+
+} else if (waether== "rainy"){
+  console.log("Better take an umbrealla");
+} else{
+  console.log("it could go either way!")
+}
---
+++ favDrinks.js
@@ -0,0 +1,11 @@
+let favDrinks=[
+
+"coke",
+"fanta",
+"tonic",
+"red bull"
+
+];
+for( let i=0; i < favDrinks.length; length++ ){
+	console.log(favDrinks[i]);
+}
